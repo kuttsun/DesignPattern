871c75a6e9a1b3ca3f3bc9a3f5fa8d64f026e878
kuttsun <tsukushimail+github@gmail.com>
2017-12-21 23:53:19 +0900

    Add Singleton.

---
+++ Singleton/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 以下はビルドエラー
+            //var singleton = new Hoge();
+
+            Hoge.Instance.DoSomething();
+
+            Console.ReadKey();
+        }
+    }
+
+    // シングルトンクラス
+    public class Hoge
+    {
+        public static Hoge Instance { get; } = new Hoge();
+
+        // private コンストラクタにすることで、new を抑制する
+        private Hoge() { }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
